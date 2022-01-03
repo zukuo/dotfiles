f818ed14cf0c660a2f2b810538a499a2700a1e9f
Zukuo <eesa@xoio.us>
2022-01-03 15:15:08 +0000

    fix font config in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,16 +5,16 @@ return {
 
     -- Font Settings
     font = wezterm.font_with_fallback({
-        {family="JetBrains Mono Nerd Font", weight="Regular"},
-        {family="Liga SFMono Nerd Font", weight="Regular"},
-        {family="Fira Code Nerd Font", weight="Regular"},
-        {family="Cascadia Code", weight="Book"},
+        {family="JetBrainsMono Nerd Font", weight="Medium"},
+        {family="Liga SFMono Nerd Font", weight="Medium"},
+        {family="FiraCode Nerd Font", weight="Medium"},
+        {family="Cascadia Code", weight="Regular", harfbuzz_features={"calt=1", "ss01=1", "ss19=1"}},
         {family="JoyPixels"},
     }),
     font_size = 12,
-    line_height = 1.3,
-    -- harfbuzz_features={"calt=1", "ss01=1", "ss19=1"}, -- enable cursive & slashed 0 (cascadia code)
+    line_height = 1.5,
     -- harfbuzz_features = {"calt=0", "clig=0", "liga=0"}, -- disable ligatures
+    -- cascadia code -> harfbuzz enables cursive & slashed 0 
 
     -- Color Setings
     color_scheme_dirs = {"colors"},
@@ -24,6 +24,7 @@ return {
     -- GUI Settings
     cursor_blink_rate = 0,
     enable_tab_bar = false,
+    use_fancy_tab_bar = false,
     enable_scroll_bar = false,
     -- window_decorations = "RESIZE", -- remove titlebar
     window_padding = {
