5699e8d9a4aec54b4de8aea47416d085d624feab
Zukuo <eesa@xoio.us>
2023-01-04 02:16:19 +0000

    Add colorizer plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,6 +5,8 @@ return {
     { "akinsho/bufferline.nvim", config = function() require("bufferline").setup() end },
     { "kyazdani42/nvim-web-devicons", config = function() require("nvim-web-devicons").setup() end },
     { "lewis6991/gitsigns.nvim", config = function() require("gitsigns").setup() end },
+    { "norcalli/nvim-colorizer.lua", config = function() require("colorizer").setup() end },
+    { "glepnir/dashboard-nvim" },
     { "psliwka/vim-smoothie" },
 
     -- Treesitter Syntax Highlighting
@@ -19,6 +21,6 @@ return {
     -- Functionality
     { "matze/vim-move" },
     { "b3nj5m1n/kommentary" },
-    { "windwp/nvim-autopairs", config = function() require("config.autopairs") end },
+    { "windwp/nvim-autopairs", config = function() require("config.autopairs").setup() end },
     { "ethanholz/nvim-lastplace", config = function() require("nvim-lastplace").setup() end },
 }
--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,4 +1,4 @@
 require("user.keymaps")
-require("user.lazy")
 require("user.options")
 require("user.autocmds")
+require("user.lazy")
