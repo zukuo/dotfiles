31831e102b897cda758a9e515a79bd01edb4f5aa
Zukuo <eesa@xoio.us>
2023-01-05 23:56:30 +0000

    Reorganise plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,15 +1,12 @@
 return {
     -- Eye Candy
     { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end },
+    { "kyazdani42/nvim-web-devicons", config = true },
     { "nvim-lualine/lualine.nvim", config = function() require("config.lualine") end },
     { "akinsho/bufferline.nvim", config = true },
-    { "kyazdani42/nvim-web-devicons", config = true },
-    { "lewis6991/gitsigns.nvim", config = true },
     { "norcalli/nvim-colorizer.lua", config = true },
-    { "goolord/alpha-nvim", config = function() require("config.alpha") end },
-    { "psliwka/vim-smoothie" },
 
-    -- Treesitter Syntax Highlighting
+    -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -18,9 +15,18 @@ return {
         config = function() require("config.treesitter") end
     },
 
-    -- Functionality
+    -- Startup Page
+    { "goolord/alpha-nvim", config = function() require("config.alpha") end },
+
+    -- Movement
+    { "psliwka/vim-smoothie" },
     { "matze/vim-move" },
-    { "b3nj5m1n/kommentary" },
-    { "windwp/nvim-autopairs", config = function() require("config.autopairs") end },
+
+    -- Editing
+    { "numToStr/Comment.nvim", config = true },
     { "ethanholz/nvim-lastplace", config = true },
+    { "windwp/nvim-autopairs", config = function() require("config.autopairs") end },
+
+    -- Git Utils
+    { "lewis6991/gitsigns.nvim", config = true },
 }
