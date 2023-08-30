f50f93a4951ab8cf4228041788696bc54ba1f5c2
Eesa Ahmad <eesa.ahmad@student.manchester.ac.uk>
2023-08-30 16:59:42 +0100

    Rearrange format of plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,6 +5,9 @@ return {
     { "folke/tokyonight.nvim" },
     { "loctvl842/monokai-pro.nvim" },
 
+    -- Set Colorscheme
+    { "LazyVim/LazyVim", opts = { colorscheme = "monokai-pro" } },
+
     -- Eye Candy
     { "kyazdani42/nvim-web-devicons", config = true },
     { "nvim-lualine/lualine.nvim", config = function() require("config.lualine") end },
@@ -12,7 +15,23 @@ return {
     { "NvChad/nvim-colorizer.lua", opts = { user_default_options = { tailwind = true } } },
     { "lukas-reineke/indent-blankline.nvim", opts = { show_trailing_blankline_indent = false } },
 
-    -- File Management
+    -- Git Utils
+    { "lewis6991/gitsigns.nvim", opts = { yadm = { enable = true } } },
+
+    -- Startup Page
+    { "goolord/alpha-nvim", config = function() require("config.alpha") end },
+
+    -- Movement
+    { "psliwka/vim-smoothie" },
+    { "matze/vim-move" },
+
+    -- Editing
+    { "numToStr/Comment.nvim", config = true },
+    { "ethanholz/nvim-lastplace", config = true },
+    { "windwp/nvim-autopairs", config = function() require("config.autopairs") end },
+    { "VonHeikemen/searchbox.nvim", dependencies = { "MunifTanjim/nui.nvim" } },
+
+    -- Treesitter (Syntax Highlighting)
     {
         "nvim-treesitter/nvim-treesitter",
         dependencies = {
@@ -29,6 +48,8 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         config = function() require("config.treesitter") end
     },
+
+    -- Telescope & File Browser
     {
         "nvim-telescope/telescope.nvim",
         dependencies = {
@@ -43,19 +64,6 @@ return {
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function() require("telescope").load_extension "file_browser" end
     },
-
-    -- Startup Page
-    { "goolord/alpha-nvim", config = function() require("config.alpha") end },
-
-    -- Movement
-    { "psliwka/vim-smoothie" },
-    { "matze/vim-move" },
-
-    -- Editing
-    { "numToStr/Comment.nvim", config = true },
-    { "ethanholz/nvim-lastplace", config = true },
-    { "windwp/nvim-autopairs", config = function() require("config.autopairs") end },
-    { "VonHeikemen/searchbox.nvim", dependencies = { "MunifTanjim/nui.nvim" } },
 
     -- LSP & Completion
     {
@@ -75,13 +83,5 @@ return {
         },
         config = function() require("config.lsp") end
     },
-
-    -- Git Utils
-    { "lewis6991/gitsigns.nvim", opts = { yadm = { enable = true } } },
-
-    -- Set Colorscheme
-    { "LazyVim/LazyVim", opts = {
-        colorscheme = "monokai-pro",
-    } },
 
 }
