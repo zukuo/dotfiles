788c9a169c535a2c9986e8e705d6d4320213e126
Eesa Ahmad <eesa.ahmad@student.manchester.ac.uk>
2023-10-25 00:40:55 +0100

    Add keybinding for lsp code formatting

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -5,6 +5,8 @@ local lsp = require('lsp-zero').preset({
 
 lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({buffer = bufnr})
+    local opts = {buffer = bufnr}
+    vim.keymap.set({'n', 'x'}, '<leader>=', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
 end)
 
 lsp.set_sign_icons({
