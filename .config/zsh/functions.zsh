3d06abea13eac3fbbdf9528e7d2a9468bc89efe3
Eesa <eesa@xoio.us>
2025-04-05 19:13:14 +0100

    Fix paths for zsh project finder function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -14,7 +14,7 @@ ce() { local file; file=$(yadm list -a | sed "s|^|$HOME\/|" | fzf) && $EDITOR $f
 cf() { local dir; dir=$(yadm ls-tree -d -r main --name-only --full-tree | sed "s|^|$HOME\/|" | fzf) && cd $dir; unset dir ; }
 
 vf() { local file; file=$(fd . --type f | fzf) && $EDITOR - $file; unset file; zle reset-prompt ; }
-gf() { local dir; dir=$(fd . $HOME/Git $HOME/UoM --type d -d 1 | fzf) && cd $dir; unset dir; zle reset-prompt ; }
+gf() { local dir; dir=$(fd . $HOME/Git --type d -d 1 | fzf) && cd $dir; unset dir; zle reset-prompt ; }
 
 # Bind Keys for Fuzzy
 zle -N gf
