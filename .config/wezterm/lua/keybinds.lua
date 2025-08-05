4cd5f17c6320c67662fd8bc728804a0f1ace2aa4
Eesa <eesa@xoio.us>
2025-08-05 21:59:41 +0100

    Add better (vim) keybinds for split panes in wezterm

--- .config/wezterm/lua/keybinds.lua
+++ .config/wezterm/lua/keybinds.lua
@@ -11,39 +11,40 @@ function module.setup(config)
 
         config.keys = {
             -- Fix Option + Arrow Key Combinations
-            { key = "LeftArrow",  mods = "OPT", action = { SendKey = { key = "b", mods = "ALT" } } },
-            { key = "RightArrow", mods = "OPT", action = { SendKey = { key = "f", mods = "ALT" } } },
+            { key = "LeftArrow",  mods = "OPT",       action = { SendKey = { key = "b", mods = "ALT" } } },
+            { key = "RightArrow", mods = "OPT",       action = { SendKey = { key = "f", mods = "ALT" } } },
 
             -- Fix Command + Arrow Key Combinations
-            { key = "LeftArrow",  mods = "CMD", action = { SendKey = { key = "Home" } } },
-            { key = "RightArrow", mods = "CMD", action = { SendKey = { key = "End" } } },
-            { key = "UpArrow",    mods = "CMD", action = { SendKey = { key = "PageUp" } } },
-            { key = "DownArrow",  mods = "CMD", action = { SendKey = { key = "PageDown" } } },
+            { key = "LeftArrow",  mods = "CMD",       action = { SendKey = { key = "Home" } } },
+            { key = "RightArrow", mods = "CMD",       action = { SendKey = { key = "End" } } },
+            { key = "UpArrow",    mods = "CMD",       action = { SendKey = { key = "PageUp" } } },
+            { key = "DownArrow",  mods = "CMD",       action = { SendKey = { key = "PageDown" } } },
 
             -- Fix Option/Command + Backspace
-            { key = "\u{8}",      mods = "OPT", action = { SendKey = { key = "W", mods = "CTRL" } } },
-            { key = "\u{8}",      mods = "CMD", action = { SendKey = { key = "U", mods = "CTRL" } } },
+            { key = "\u{8}",      mods = "OPT",       action = { SendKey = { key = "W", mods = "CTRL" } } },
+            { key = "\u{8}",      mods = "CMD",       action = { SendKey = { key = "U", mods = "CTRL" } } },
 
             -- Custom Events
-            { key = "b",          mods = "CMD", action = wezterm.action.EmitEvent "toggle-bar" },
-            { key = "P",          mods = "CMD", action = wezterm.action.ActivateCommandPalette },
+            { key = "b",          mods = "CMD",       action = wezterm.action.EmitEvent "toggle-bar" },
+            { key = "P",          mods = "CMD",       action = wezterm.action.ActivateCommandPalette },
 
             -- Window Splits
-            {
-                key = "-",
-                mods = "CMD|SHIFT",
-                action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-            },
-            {
-                key = "w",
-                mods = "CMD",
-                action = wezterm.action.CloseCurrentPane { confirm = true },
-            },
-            {
-                key = ';',
-                mods = 'CMD',
-                action = wezterm.action.TogglePaneZoomState,
-            },
+            { key = "-",          mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+            { key = "\\",         mods = "CMD|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+            { key = "w",          mods = "CMD",       action = wezterm.action.CloseCurrentPane { confirm = true } },
+            { key = ';',          mods = 'CMD',       action = wezterm.action.TogglePaneZoomState },
+
+            -- Vim Movement for Panes
+            { key = 'j',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection('Down') },
+            { key = 'k',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection('Up') },
+            { key = 'h',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection('Left') },
+            { key = 'l',          mods = 'CMD',       action = wezterm.action.ActivatePaneDirection('Right') },
+
+            -- Pane Adjustment
+            { key = 'j',          mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Down', 5 } },
+            { key = 'k',          mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Up', 5 } },
+            { key = 'h',          mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Left', 5 } },
+            { key = 'l',          mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Right', 5 } },
         }
     else
         --== LINUX KEYBINDS ==--
