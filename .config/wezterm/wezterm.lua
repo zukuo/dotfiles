2f08247c8409522666346c542847f4bfe2168789
Eesa <eesa@xoio.us>
2025-08-15 21:36:51 +0100

    refactor(wezterm): major minimalisation of the config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,55 +10,45 @@ end
 config = {
     -- Font Settings
     font = wezterm.font_with_fallback({
+        {family="Berkeley Mono", weight="Regular"},
         {family="JetBrains Mono", weight="Regular"},
         {family="Cascadia Code", weight="Regular", harfbuzz_features={"calt=1", "ss01=1", "ss19=1"}},
         {family="Fira Code", weight="Regular"},
         {family="IBM Plex Mono", weight="Medium"},
         {family="JoyPixels"},
     }),
-    font_size = 15,
-    line_height = 1.3,
-    cursor_thickness = "175%",
+    font_size = 16,
+    line_height = 1.2,
+    -- cursor_thickness = "175%",
 
     -- Rendering
-    front_end = "WebGpu",
-    webgpu_power_preference = "HighPerformance",
+    -- front_end = "WebGpu",
+    -- webgpu_power_preference = "HighPerformance",
     max_fps = 240,
     animation_fps = 240,
 
+    -- TODO: make a global colorscheme property for terminal/prompt
+
     -- Color Setings
     color_scheme = 'Default Dark (base16)',
-    tab_color_scheme = 'base16', -- 'monokaipro', 'tokyonight', 'base16', 'blackwhite'
-    window_background_opacity = 0.95,
-    macos_window_background_blur = 20,
+    colors = { background = "#141415" },
 
     -- GUI Settings
     cursor_blink_rate = 0,
-    enable_tab_bar = true,
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    tab_max_width = 100,
+    enable_tab_bar = false,
     enable_scroll_bar = false,
     adjust_window_size_when_changing_font_size = false,
-    -- hide_tab_bar_if_only_one_tab = true,
     window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW",
     window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
+        left = 20,
+        right = 20,
+        top = 10,
+        bottom = 10,
     },
 }
 
 -- Setup Keybinds
 require("lua.keybinds").setup(config)
-
--- Setup WezSSH
-require("lua.ssh").setup(config)
-
--- Setup Custom Tabs & Statusline
-require("lua.tabs").setup(config)
-require("lua.status").setup(config)
 
 -- Setup Overrides
 require("lua.overrides").setup(config)
