594a9f905c070f82d63b537a47b04b7e2dc73b9c
Eesa <eesa@xoio.us>
2025-11-03 14:33:53 +0000

    fix: make quit bind delete compilation buffer

--- .config/nvim/lua/plugins/compile.lua
+++ .config/nvim/lua/plugins/compile.lua
@@ -16,6 +16,12 @@ return {
 		{ "<leader>cs", "<cmd>below Compile<cr>" },
 		{ "<leader>cc", "<cmd>below Recompile<cr>" },
 		{ "<leader>cn", "<cmd>NextError<cr>" },
-		{ "<leader>cq", function() require("compile-mode").close_buffer() end }, -- TODO: make it delete the buffer
+		{ "<leader>cq", function()
+			local buf = vim.g.compilation_buffer
+			if buf and vim.api.nvim_buf_get_name(buf) then
+				require("compile-mode").close_buffer()
+				vim.api.nvim_buf_delete(buf, { force = true })
+			end
+		end },
 	},
 }
