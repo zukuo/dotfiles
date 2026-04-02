a7db5b0b2bdcfabfe24a7fca8ad079ae631431af
Eesa <eesa@xoio.us>
2026-04-02 21:39:43 +0100

    fix: fix c commenting and for non-treesitter parsed files

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,12 +1,12 @@
 return {
 	{
-		"numToStr/Comment.nvim",
+		"faergeek/Comment.nvim", -- TODO: update to a newer stable branch
+		branch = "nvim-0.12-compatibility",
 		config = function()
 			local prehook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 			require("Comment").setup({
 				ignore = "^$", -- ignore blank lines
 				pre_hook = prehook, -- for typescript react commenting
-				post_hook = nil,
 			})
 		end,
 		event = "BufReadPre",
@@ -15,5 +15,16 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+				languages = {
+					c = { __default = "// %s", __multiline = "/* %s */" }, -- fix c commenting
+				},
+			})
+		end,
 	},
 }
